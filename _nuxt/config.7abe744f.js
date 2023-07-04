1b7e490033bcdc0dac295bec19494c4cf084708d
Kamsi-yonna <122364352+Kamsi-yonna@users.noreply.github.com>
2023-07-04 23:08:58 +0000

    Deploying to gh-pages from @ Kamsi-yonna/nuxtDailyScoop@54ffd514d340d4ba6a067ae8fb09a170d35cb4ac 🚀

---
+++ _nuxt/config.7abe744f.js
@@ -0,0 +1,1 @@
+import{J as p,A as o,v as i}from"./entry.23ae00fc.js";const f={},t=p(f);function s(){const n=i();return n._appConfig||(n._appConfig=o(t)),n._appConfig}export{s as u};
--- _nuxt/config.c077a76a.js
+++
@@ -1,1 +0,0 @@
-import{I as p,z as o,s as i}from"./entry.dd4d7170.js";const f={},s=p(f);function a(){const n=i();return n._appConfig||(n._appConfig=o(s)),n._appConfig}export{a as u};
